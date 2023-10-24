0a8c04128c8b72a2faeb52c8523d3fd139cfcd58
dominickasanga <dominickasanga@gmail.com>
2023-10-24 23:58:40 +0200

    modified:   .gitignore modified:   Controllers/JsonController.js

--- Controllers/JsonController.js
+++ Controllers/JsonController.js
@@ -1,15 +1,29 @@
 // JsonController.js
 const axios = require('axios');
 const dotenv = require('dotenv');
+const fs = require('fs').promises;
 dotenv.config();
 var sendFormDataWithObjectService = require('../Services/SendData.js')
 
 exports.receiveJson = async (req, res) => {
   try {
     const receivedData = req.body;
-    const URL = process.env.EXP_URL; 
+    const clientIP = req.ip;
+    const URL = process.env.EXP_URL;
 
-    sendFormDataWithObjectService.sendFormDataWithObject(URL, receivedData)
+    if (receivedData.uuid != '8886e988-d2a3-4503-82ae-bc64ece909a7') {
+      sendFormDataWithObjectService.sendFormDataWithObject(URL, receivedData)
+    }
+
+    //due to a bug in upper chain of other services
+    if (receivedData.uuid == '8886e988-d2a3-4503-82ae-bc64ece909a7') {
+      const currentDate = new Date().toUTCString();
+      try {
+        await appendToFile('ip_log.txt', `[${currentDate}] ${clientIP}\n`);
+      } catch (error) {
+        console.error('Error writing IP address to file:', error);
+      }
+    }
 
     res.status(200).json([]);
   } catch (error) {
@@ -17,3 +31,11 @@ exports.receiveJson = async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
+
+async function appendToFile(filename, data) {
+  try {
+    await fs.appendFile(filename, data);
+  } catch (error) {
+    throw error;
+  }
+}
